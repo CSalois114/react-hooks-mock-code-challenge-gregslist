3a76be0bda845752a96cc98087194fe262df329f
Chris Salois <csalois@gmail.com>
2022-05-04 10:57:28 -0400

    core deliverables working

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+  let [listings, setListings] = useState([]);
+  const [searchBy, setSearchBy] = useState("");
+
+  // get listings from db
+  useEffect(() => {
+    fetch("http://localhost:6001/listings")
+    .then((res) => res.json())
+    .then(setListings);
+  }, []);
+  
+  const changeSearch = (newSearch) => setSearchBy(newSearch);
+  const delListing = (delListing) => {
+    fetch(`http://localhost:6001/listings/${delListing.id}`,
+    { method: "DELETE"})
+    .then(res => res.json())
+    .then(data => { if(typeof data === 'object' && !Object.keys(data)[0]) {
+      setListings(listings.filter(listing => listing !== delListing))
+    }})  
+  }
+  
+  // If there is a search term entered filter listings by it
+  searchBy && (listings = listings.filter((listing) =>
+    listing.description.toLowerCase().includes(searchBy)
+  ));
+
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer />
+      <Header changeSearch={changeSearch} />
+      <ListingsContainer listings={listings} delListing={delListing} />
     </div>
   );
 }
--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,16 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
+import ListingCard from "./ListingCard";
 
-function ListingsContainer() {
+function ListingsContainer({ listings, delListing }) {
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {listings.map((listing) => (
+          <ListingCard
+            key={listing.id}
+            listing={listing}
+            delListing={delListing}
+          />
+        ))}
       </ul>
     </main>
   );
